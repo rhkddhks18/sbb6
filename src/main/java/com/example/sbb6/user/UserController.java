cac444fc476fe4e3480631dd03fc2fc1f2911871
rhkddhks18 <taengja18@naver.com>
2023-11-30 13:46:18 +0900

    예외처리

--- src/main/java/com/example/sbb6/user/UserController.java
+++ src/main/java/com/example/sbb6/user/UserController.java
@@ -2,6 +2,7 @@ package com.example.sbb6.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,7 +30,18 @@ public class UserController {
                     "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
         }
-        userService.create(userCreateForm.getEmailId(), userCreateForm.getNickname(), userCreateForm.getPassword1());
+        try {
+            userService.create(userCreateForm.getEmailId(),
+                    userCreateForm.getNickname(), userCreateForm.getPassword1());
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        }catch(Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
 
         return "redirect:/";
     }
